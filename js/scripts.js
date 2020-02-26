14e46c532b48a44ea2df3afdb2eb1233ac226635
stephaniehenry <46508253+stephaniehenry@users.noreply.github.com>
2020-02-25 21:58:24 -0800

    Updated Scripts
    Added first batch of JS to make the hamster "run"

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log('OK');
+// gsap.to
+//   ('.wheel', {rotate: 360, repeat: -1, duration: .15 });
+
+gsap.to 
+  ('.hamster', {x: 5, y: -5, rotate: 10, ease:"", repeat:-1, duration: .15, yoyo: true});
+
+gsap.to
+ ('#ear', {x: -5, y: 5, rotate: 10, ease:"", repeat: -1, duration: .15, yoyo: true});  
+
+gsap.to
+ ('#eye', {x: -5, y: 10, rotate: 10, ease:"", repeat: -1, duration: .15, yoyo: true});
