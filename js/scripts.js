ad881206c68ca7536205235d990592f4b871edaa
stephaniehenry <46508253+stephaniehenry@users.noreply.github.com>
2020-02-26 14:40:54 -0800

    Updated js
    The wheel now spins!

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,11 @@ gsap.to
 
 gsap.to
  ('#eye', {x: -5, y: 10, rotate: 10, ease:"", repeat: -1, duration: .15, yoyo: true});
+
+gsap.to( '.wheel', {
+  rotate: 360,
+  repeat: -1,
+  transformOrigin: '50% 50%',
+  ease:"linear",
+  duration: 1
+  });
